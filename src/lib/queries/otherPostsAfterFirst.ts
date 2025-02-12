a7921b23cbdf235c915b6e0fc32dc9ab8fbb2671
James Winfield <hello@iwillbeawebdeveloper.co.uk>
2025-02-12 19:18:29 +0000

    index page

--- src/lib/queries/otherPostsAfterFirst.ts
+++ src/lib/queries/otherPostsAfterFirst.ts
@@ -1,6 +1,6 @@
 const OTHER_POSTS_AFTER_FIRST_QUERY = `
   query GetOtherPosts($after: String) {
-    posts(first: 13, after: $after) {
+    posts(first: 7, after: $after) {
       nodes {
         id
         slug
