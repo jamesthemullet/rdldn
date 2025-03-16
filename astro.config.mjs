3beef46a9173b31bccf38e225ce03f73949815c9
James Winfield <hello@iwillbeawebdeveloper.co.uk>
2025-03-16 10:27:54 +0000

    add search functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,14 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import partytown from "@astrojs/partytown";
 
+import alpinejs from "@astrojs/alpinejs";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-    mdx(),
-    sitemap(),
-  ],
+  integrations: [partytown({
+    config: {
+      forward: ["dataLayer.push"],
+    },
+  }), mdx(), sitemap(), alpinejs()],
 });
