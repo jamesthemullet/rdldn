2fd535e5adf7500eb2bb232f432620f4cc4f9e68
James Winfield <hello@iwillbeawebdeveloper.co.uk>
2025-04-07 18:55:15 +0100

    biome fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,22 @@
 // @ts-check
-import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
 import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [partytown({
-    config: {
-      forward: ["dataLayer.push"],
-    },
-  }), mdx(), sitemap(), alpinejs()],
+  integrations: [
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+    mdx(),
+    sitemap(),
+    alpinejs(),
+  ],
 });
