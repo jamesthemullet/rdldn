2064c9226195edbe673b7fedd9c91cb8c3aaa7c9
James Winfield <hello@jameswinfield.co.uk>
2026-01-14 07:13:05 +0000

    ts fixes

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,31 +2,29 @@ import path from "node:path";
 import { pathToFileURL } from "node:url";
 import { defineConfig } from "vitest/config";
 
-export default defineConfig(async () => {
-  const astroRoot = path.join(process.cwd(), "node_modules", "astro", "dist");
-  const [{ default: astroPlugin }, config] = await Promise.all([
-    import(pathToFileURL(path.join(astroRoot, "vite-plugin-astro", "index.js")).href),
-    import(pathToFileURL(path.join(astroRoot, "core", "config", "index.js")).href)
-  ]);
+const astroRoot = path.join(process.cwd(), "node_modules", "astro", "dist");
 
-  const root = process.cwd();
-  const { astroConfig } = await config.resolveConfig({ root }, "dev");
-  const settings = await config.createSettings(astroConfig, root);
-  const logger = config.createNodeLogger(astroConfig);
+const [{ default: astroPlugin }, config] = await Promise.all([
+  import(pathToFileURL(path.join(astroRoot, "vite-plugin-astro", "index.js")).href),
+  import(pathToFileURL(path.join(astroRoot, "core", "config", "index.js")).href)
+]);
 
-  return {
-    plugins: [astroPlugin({ settings, logger })],
-    test: {
-      globals: true,
-      environment: "node",
-      include: ["src/**/*.{test,spec}.{ts,tsx}"],
-      coverage: {
-        provider: "istanbul",
-        all: true,
-        include: ["src/**/*.{ts,tsx,astro}"],
-        reporter: ["text", "html", "lcov"],
-        reportsDirectory: "coverage"
-      }
+const root = process.cwd();
+const { astroConfig } = await config.resolveConfig({ root }, "dev");
+const settings = await config.createSettings(astroConfig, root);
+const logger = config.createNodeLogger(astroConfig);
+
+export default defineConfig({
+  plugins: [astroPlugin({ settings, logger })],
+  test: {
+    globals: true,
+    environment: "node",
+    include: ["src/**/*.{test,spec}.{ts,tsx}"],
+    coverage: {
+      provider: "istanbul",
+      include: ["src/**/*.{ts,tsx,astro}"],
+      reporter: ["text", "html", "lcov"],
+      reportsDirectory: "coverage"
     }
-  };
+  }
 });
