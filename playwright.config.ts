2c77b7df25f4247497a5ae34fba9ac95eafb64b1
James Winfield <hello@jameswinfield.co.uk>
2026-01-14 20:31:00 +0000

    add playwright

---
+++ playwright.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./tests/e2e",
+  fullyParallel: true,
+  reporter: "list",
+  use: {
+    baseURL: "http://localhost:4321",
+    trace: "on-first-retry",
+    headless: true,
+  },
+  webServer: {
+    command: "yarn dev",
+    url: "http://localhost:4321",
+    reuseExistingServer: true,
+    timeout: 120000,
+  },
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
+  ],
+});
