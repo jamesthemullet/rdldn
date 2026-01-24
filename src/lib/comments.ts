b757bf36d9ecd89350d673dde7b0c74f3209e211
James Winfield <hello@jameswinfield.co.uk>
2026-01-24 12:32:51 +0000

    organise comments

---
+++ src/lib/comments.ts
@@ -0,0 +1,1 @@
+export { organiseComments } from "./utils";
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,23 +1,29 @@
-import type { Comments as CommentsType } from "../types";
+import type { Comment, Comments as CommentsType } from "../types";
 
-export const organiseComments = (comments: CommentsType) => {
-  const commentMap = new Map();
+export const organiseComments = (comments: CommentsType): Comment[] => {
+  if (!comments || comments.length === 0) {
+    return [];
+  }
 
-  comments?.forEach((comment) => {
+  const commentMap = new Map<string, Comment>();
+
+  comments.forEach((comment) => {
     comment.replies = [];
     commentMap.set(comment.id, comment);
   });
 
-  const topLevelComments: CommentsType = [];
-  comments?.forEach((comment) => {
+  const topLevelComments: Comment[] = [];
+
+  comments.forEach((comment) => {
     if (comment.parentId) {
       const parent = commentMap.get(comment.parentId);
       if (parent) {
-        parent.replies.push(comment);
+        parent.replies?.push(comment);
+        return;
       }
-    } else {
-      topLevelComments.push(comment);
     }
+
+    topLevelComments.push(comment);
   });
 
   return topLevelComments;
