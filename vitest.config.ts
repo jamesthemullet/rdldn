688e6b566cc426762c8b8dedb927d8f5b3f267c8
James Winfield <hello@jameswinfield.co.uk>
2026-03-30 20:28:50 +0100

    revert

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,15 +4,16 @@ import { defineConfig } from "vitest/config";
 
 const astroRoot = path.join(process.cwd(), "node_modules", "astro", "dist");
 
-const [{ default: astroPlugin }, config] = await Promise.all([
+const [{ default: astroPlugin }, config, { createNodeLogger }] = await Promise.all([
   import(pathToFileURL(path.join(astroRoot, "vite-plugin-astro", "index.js")).href),
-  import(pathToFileURL(path.join(astroRoot, "core", "config", "index.js")).href)
+  import(pathToFileURL(path.join(astroRoot, "core", "config", "index.js")).href),
+  import(pathToFileURL(path.join(astroRoot, "core", "logger", "node.js")).href)
 ]);
 
 const root = process.cwd();
 const { astroConfig } = await config.resolveConfig({ root }, "dev");
 const settings = await config.createSettings(astroConfig, astroConfig.logLevel, root);
-const logger = config.createNodeLogger(astroConfig);
+const logger = createNodeLogger(astroConfig);
 
 export default defineConfig({
   plugins: [astroPlugin({ settings, logger })],
