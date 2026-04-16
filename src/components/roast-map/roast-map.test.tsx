b0db33ce0e609601987bde854cbd0d5a2831c9fe
James Winfield <hello@jameswinfield.co.uk>
2026-04-16 20:12:56 +0100

    mock usememo

--- src/components/roast-map/roast-map.test.tsx
+++ src/components/roast-map/roast-map.test.tsx
@@ -271,6 +271,7 @@ describe("roast-map component", () => {
         ...actual,
         useRef: () => ({ current: null }),
         useState: <T,>(initialValue: T) => [initialValue, vi.fn()],
+        useMemo: <T,>(fn: () => T) => fn(),
         useEffect: (callback: () => unknown) => {
           callback();
         }
