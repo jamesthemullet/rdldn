319e9ffdf0f89cdb9b50b05deec0db4d705e3a3b
James Winfield <hello@jameswinfield.co.uk>
2026-04-27 20:14:16 +0100

    lint fix

--- src/astro-modules.d.ts
+++ src/astro-modules.d.ts
@@ -1,5 +1,6 @@
 declare module "*.astro" {
   import type { AstroComponentFactory } from "astro/runtime/server/index.js";
+
   const Component: AstroComponentFactory;
   export default Component;
   export const getStaticPaths: (...args: unknown[]) => unknown;
